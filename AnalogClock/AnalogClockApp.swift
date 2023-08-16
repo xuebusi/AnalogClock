1eb73f779bba910cd3e277a0cfba45c2fc2c3a1d
xuebusi <xuebusi@qq.com>
2023-08-16 20:46:29 +0800

    init

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by shiyanjun on 2023/8/16.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
